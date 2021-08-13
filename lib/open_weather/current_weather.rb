07adf2182ccd52a1de71491a8f0917ddfbc49ab7
code salley <juxsalley@gmail.com>
2021-08-13 22:53:52 +0000

    seperate logic to reduce code dublication

--- lib/open_weather/current_weather.rb
+++ lib/open_weather/current_weather.rb
@@ -1,86 +1,22 @@
-require 'httparty'
-require 'json'
-require_relative 'helpers/helpers'
-require 'logger'
+require "httparty"
+require "json"
+require_relative "helpers/helpers"
+require "logger"
 
 module OpenWeather
   class CurrentWeather
     @helper = Helpers.new
-    def self.by_city(name: '', units: 'standard', lang: 'en')
-      url = OpenWeather.configuration.weather_base_uri
-      api_key = OpenWeather.configuration.api_key
-      uri = @helper.name_to_url(url, name, api_key, units, lang)
-      begin
-        @response = HTTParty.get(uri)
-      rescue StandardError => e
-        logger = Logger.new($stdout)
-        logger.error('Network is required')
-        return []
-      end
-      if @response.message == 'Unauthorized' || @response.code == 404
-        logger = Logger.new($stdout)
-        logger.error('Api Key is required')
-        return []
-      end
-      @helper.to_json(@response.body)
+    @url = "pro.openweathermap.org/data/2.5/forecast/hourly?"
+    def self.by_city(name: "", units: "standard", lang: "en")
     end
 
-    def self.by_city_id(id: '', units: 'standard', lang: 'en')
-      url = OpenWeather.configuration.weather_base_uri
-      api_key = OpenWeather.configuration.api_key
-      uri = @helper.id_to_url(url, id, api_key, units, lang)
-      begin
-        @response = HTTParty.get(uri)
-      rescue StandardError => e
-        logger = Logger.new($stdout)
-        logger.error('Network is required')
-        return []
-      end
-      if @response.message == 'Unauthorized' || @response.code == 404
-        logger = Logger.new($stdout)
-        logger.error('Api Key is required')
-        return []
-      end
-      @helper.to_json(@response.body)
+    def self.by_city_id(id: "", units: "standard", lang: "en")
     end
 
-    def self.by_coords(coords: [], units: 'standard', lang: 'en')
-      url = OpenWeather.configuration.weather_base_uri
-      api_key = OpenWeather.configuration.api_key
-      uri = @helper.cords_to_url(url, coords, api_key, units, lang)
-
-      begin
-        @response = HTTParty.get(uri)
-      rescue StandardError => e
-        logger = Logger.new($stdout)
-        logger.error('Network is required')
-        return []
-      end
-      if @response.message == 'Unauthorized' || @response.code == 404
-        logger = Logger.new($stdout)
-        logger.error('Api Key is required')
-        return []
-      end
-      @helper.to_json(@response.body)
+    def self.by_coords(coords: [], units: "standard", lang: "en")
     end
 
-    def self.by_zip(zipcode: '', country: '', units: 'standard', lang: 'en')
-      url = OpenWeather.configuration.weather_base_uri
-      api_key = OpenWeather.configuration.api_key
-      uri = @helper.zipcode_to_url(url, zipcode, country, api_key, units, lang)
-      begin
-        @response = HTTParty.get(uri)
-      rescue StandardError => e
-        logger = Logger.new($stdout)
-        logger.error('Network is required')
-        return []
-      end
-      if @response.message == 'Unauthorized' || @response.code == 404
-        logger = Logger.new($stdout)
-        logger.error('Api Key is required')
-        return []
-      end
-      @helper.to_json(@response.body)
+    def self.by_zip(zipcode: "", country: "", units: "standard", lang: "en")
     end
   end
 end
