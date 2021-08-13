5d6b84edcb875004f32051645b77b4e156907fd6
code salley <juxsalley@gmail.com>
2021-08-13 17:55:27 +0000

    use name arguments instead of positional

--- lib/open_weather/current_weather.rb
+++ lib/open_weather/current_weather.rb
@@ -1,33 +1,65 @@
 require "httparty"
 require "json"
 require_relative "helpers/helpers"
+require "logger"
 
 module OpenWeather
   class CurrentWeather
     @helper = Helpers.new
-    def self.by_city(name, units = "standard", lang = "en")
-      @url = OpenWeather.configuration.weather_base_uri
-      @api_key = OpenWeather.configuration.api_key
-      uri = @helper.to_url_string(@url, name, @api_key, units, lang)
+    def self.by_city(name: "", units: "standard", lang: "en")
+      url = OpenWeather.configuration.weather_base_uri
+      api_key = OpenWeather.configuration.api_key
+      uri = @helper.name_to_url(url, name, api_key, units, lang)
       response = HTTParty.get(uri)
+      if response.message == "Unauthorized" || response.code == 404
+        logger = Logger.new(STDOUT)
+        logger.error("Api Key is required")
+        return []
+      end
       res = @helper.to_json(response.body)
       return res
     end
 
-    def self.by_city_id(id)
-      @url = OpenWeather.configuration.weather_base_uri
-      @api_key = OpenWeather.configuration.api_key
-      uri = @helper.to_id_string(@url, id, @api_key, units, lang)
+    def self.by_city_id(id: "", units: "standard", lang: "en")
+      url = OpenWeather.configuration.weather_base_uri
+      api_key = OpenWeather.configuration.api_key
+      uri = @helper.id_to_url(url, id, api_key, units, lang)
+      response = HTTParty.get(uri)
+      if response.message == "Unauthorized" || response.code == 404
+        logger = Logger.new(STDOUT)
+        logger.error("Api Key is required")
+        return []
+      end
       res = @helper.to_json(response.body)
       return res
     end
 
-    def self.by_coords
-      return "by cords"
+    def self.by_coords(cords: [], units: "standard", lang: "en")
+      url = OpenWeather.configuration.weather_base_uri
+      api_key = OpenWeather.configuration.api_key
+      uri = @helper.cords_to_url(url, cords, api_key, units, lang)
+      response = HTTParty.get(uri)
+      if response.message == "Unauthorized" || response.code == 404
+        logger = Logger.new(STDOUT)
+        logger.error("Api Key is required")
+        return []
+      end
+      res = @helper.to_json(response.body)
+      return res
     end
 
-    def self.by_zip
-      return "by zip"
+    def self.by_zip(zipcode: "", country: "", units: "" "standard", lang: "en")
+      url = OpenWeather.configuration.weather_base_uri
+      api_key = OpenWeather.configuration.api_key
+      uri = @helper.zipcode_to_url(url, zipcode, country, api_key, units, lang)
+      response = HTTParty.get(uri)
+      if response.message == "Unauthorized" || response.code == 404
+        logger = Logger.new(STDOUT)
+        logger.error("Api Key is required")
+        return []
+      end
+      res = @helper.to_json(response.body)
+      return res
     end
   end
 end
--- lib/open_weather/helpers/helpers.rb
+++ lib/open_weather/helpers/helpers.rb
@@ -12,8 +12,23 @@ class Helpers
     res
   end
 
-  def to_url_string(url, name, apiKey, units, lang)
+  def name_to_url(url, name, apiKey, units, lang)
     res = "#{url}q=#{name}&appid=#{apiKey}&lang=#{lang}&units=#{units}"
+    return res
+  end
+
+  def id_to_url(url, name, apiKey, units, lang)
+    res = "#{url}id=#{name}&appid=#{apiKey}&lang=#{lang}&units=#{units}"
+    return res
+  end
+
+  def cords_to_url(url, coords, apiKey, units, lang)
+    res = "#{url}lat=#{coords[1]}&lon=#{coords[0]}&appid=#{apiKey}&lang=#{lang}&units=#{units}"
+    return res
+  end
+
+  def zipcode_to_url(url, zip, country, apiKey, units, lang)
+    res = "#{url}zip=#{zip},#{country}&appid=#{apiKey}&lang=#{lang}&units=#{units}"
     return res
   end
 end
