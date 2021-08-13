372f90d0747eac1160303efb4d8d457649a5ac83
code salley <juxsalley@gmail.com>
2021-08-13 11:12:47 +0000

    branch init

---
+++ spec/open_weather_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe OpenWeather do
+  it "has a version number" do
+    expect(OpenWeather::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
